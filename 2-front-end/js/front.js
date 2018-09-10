b7b1c2c171004d81aa514e67fc612f077aa5f3ad
Andrew GALAVAN <agalavan@student.unit.ua>
2018-09-10 17:06:39 +0300

    add edit photo html+css not complited

--- 2-front-end/js/front.js
+++ 2-front-end/js/front.js
@@ -1,177 +1,179 @@
 $(document).ready(function () {
 
-    'use strict';
+	'use strict';
 
-    // ------------------------------------------------------- //
-    // Search Box
-    // ------------------------------------------------------ //
-    $('#search').on('click', function (e) {
-        e.preventDefault();
-        $('.search-box').fadeIn();
-    });
-    $('.dismiss').on('click', function () {
-        $('.search-box').fadeOut();
-    });
+	// ------------------------------------------------------- //
+	// Search Box
+	// ------------------------------------------------------ //
+	$('#search').on('click', function (e) {
+		e.preventDefault();
+		$('.search-box').fadeIn();
+	});
+	$('.dismiss').on('click', function () {
+		$('.search-box').fadeOut();
+	});
 
-    // ------------------------------------------------------- //
-    // Card Close
-    // ------------------------------------------------------ //
-    $('.card-close a.remove').on('click', function (e) {
-        e.preventDefault();
-        $(this).parents('.card').fadeOut();
-    });
+	// ------------------------------------------------------- //
+	// Card Close
+	// ------------------------------------------------------ //
+	$('.card-close a.remove').on('click', function (e) {
+		e.preventDefault();
+		$(this).parents('.card').fadeOut();
+	});
 
-    // ------------------------------------------------------- //
-    // Tooltips init
-    // ------------------------------------------------------ //    
+	// ------------------------------------------------------- //
+	// Tooltips init
+	// ------------------------------------------------------ //    
 
-    $('[data-toggle="tooltip"]').tooltip()    
-
-
-    // ------------------------------------------------------- //
-    // Adding fade effect to dropdowns
-    // ------------------------------------------------------ //
-    $('.dropdown').on('show.bs.dropdown', function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
-    });
-    $('.dropdown').on('hide.bs.dropdown', function () {
-        $(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
-    });
+	$('[data-toggle="tooltip"]').tooltip()    
 
 
-    // ------------------------------------------------------- //
-    // Sidebar Functionality
-    // ------------------------------------------------------ //
-    $('#toggle-btn').on('click', function (e) {
-        e.preventDefault();
-        $(this).toggleClass('active');
+	// ------------------------------------------------------- //
+	// Adding fade effect to dropdowns
+	// ------------------------------------------------------ //
+	$('.dropdown').on('show.bs.dropdown', function () {
+		$(this).find('.dropdown-menu').first().stop(true, true).fadeIn();
+	});
+	$('.dropdown').on('hide.bs.dropdown', function () {
+		$(this).find('.dropdown-menu').first().stop(true, true).fadeOut();
+	});
 
-        $('.side-navbar').toggleClass('shrinked');
-        $('.content-inner').toggleClass('active');
-        $(document).trigger('sidebarChanged');
 
-        if ($(window).outerWidth() > 1183) {
-            if ($('#toggle-btn').hasClass('active')) {
-                $('.navbar-header .brand-small').hide();
-                $('.navbar-header .brand-big').show();
-            } else {
-                $('.navbar-header .brand-small').show();
-                $('.navbar-header .brand-big').hide();
-            }
-        }
+	// ------------------------------------------------------- //
+	// Sidebar Functionality
+	// ------------------------------------------------------ //
+	$('#toggle-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
 
-        if ($(window).outerWidth() < 1183) {
-            $('.navbar-header .brand-small').show();
-        }
-    });
+		$('.side-navbar').toggleClass('shrinked');
+		$('.content-inner').toggleClass('active');
+		$(document).trigger('sidebarChanged');
 
-    // ------------------------------------------------------- //
-    // Universal Form Validation
-    // ------------------------------------------------------ //
+		if ($(window).outerWidth() > 1183) {
+			if ($('#toggle-btn').hasClass('active')) {
+				$('.navbar-header .brand-small').hide();
+				$('.navbar-header .brand-big').show();
+			} else {
+				$('.navbar-header .brand-small').show();
+				$('.navbar-header .brand-big').hide();
+			}
+		}
 
-    $('.form-validate').each(function() {  
-        $(this).validate({
-            errorElement: "div",
-            errorClass: 'is-invalid',
-            validClass: 'is-valid',
-            ignore: ':hidden:not(.summernote, .checkbox-template, .form-control-custom),.note-editable.card-block',
-            errorPlacement: function (error, element) {
-                // Add the `invalid-feedback` class to the error element
-                error.addClass("invalid-feedback");
-                console.log(element);
-                if (element.prop("type") === "checkbox") {
-                    error.insertAfter(element.siblings("label"));
-                } 
-                else {
-                    error.insertAfter(element);
-                }
-            }
-        });
+		if ($(window).outerWidth() < 1183) {
+			$('.navbar-header .brand-small').show();
+		}
+	});
 
-    });    
+	// ------------------------------------------------------- //
+	// Universal Form Validation
+	// ------------------------------------------------------ //
 
-    // ------------------------------------------------------- //
-    // Material Inputs
-    // ------------------------------------------------------ //
+	$('.form-validate').each(function() {  
+		$(this).validate({
+			errorElement: "div",
+			errorClass: 'is-invalid',
+			validClass: 'is-valid',
+			ignore: ':hidden:not(.summernote, .checkbox-template, .form-control-custom),.note-editable.card-block',
+			errorPlacement: function (error, element) {
+				// Add the `invalid-feedback` class to the error element
+				error.addClass("invalid-feedback");
+				console.log(element);
+				if (element.prop("type") === "checkbox") {
+					error.insertAfter(element.siblings("label"));
+				} 
+				else {
+					error.insertAfter(element);
+				}
+			}
+		});
 
-    var materialInputs = $('input.input-material');
+	});    
 
-    // activate labels for prefilled values
-    materialInputs.filter(function() { return $(this).val() !== ""; }).siblings('.label-material').addClass('active');
+	// ------------------------------------------------------- //
+	// Material Inputs
+	// ------------------------------------------------------ //
 
-    // move label on focus
-    materialInputs.on('focus', function () {
-        $(this).siblings('.label-material').addClass('active');
-    });
+	var materialInputs = $('input.input-material');
 
-    // remove/keep label on blur
-    materialInputs.on('blur', function () {
-        $(this).siblings('.label-material').removeClass('active');
+	// activate labels for prefilled values
+	materialInputs.filter(function() { return $(this).val() !== ""; }).siblings('.label-material').addClass('active');
 
-        if ($(this).val() !== '') {
-            $(this).siblings('.label-material').addClass('active');
-        } else {
-            $(this).siblings('.label-material').removeClass('active');
-        }
-    });
+	// move label on focus
+	materialInputs.on('focus', function () {
+		$(this).siblings('.label-material').addClass('active');
+	});
 
-    // ------------------------------------------------------- //
-    // Footer 
-    // ------------------------------------------------------ //   
+	// remove/keep label on blur
+	materialInputs.on('blur', function () {
+		$(this).siblings('.label-material').removeClass('active');
 
-    var contentInner = $('.content-inner');
+		if ($(this).val() !== '') {
+			$(this).siblings('.label-material').addClass('active');
+		} else {
+			$(this).siblings('.label-material').removeClass('active');
+		}
+	});
 
-    $(document).on('sidebarChanged', function () {
-        adjustFooter();
-    });
+	// ------------------------------------------------------- //
+	// Footer 
+	// ------------------------------------------------------ //   
 
-    $(window).on('resize', function () {
-        adjustFooter();
-    })
+	var contentInner = $('.content-inner');
 
-    function adjustFooter() {
-        var footerBlockHeight = $('.main-footer').outerHeight();
-        contentInner.css('padding-bottom', footerBlockHeight + 'px');
-    }
+	$(document).on('sidebarChanged', function () {
+		adjustFooter();
+	});
 
-    // ------------------------------------------------------- //
-    // External links to new window
-    // ------------------------------------------------------ //
-    $('.external').on('click', function (e) {
+	$(window).on('resize', function () {
+		adjustFooter();
+	})
 
-        e.preventDefault();
-        window.open($(this).attr("href"));
-    });
+	function adjustFooter() {
+		var footerBlockHeight = $('.main-footer').outerHeight();
+		contentInner.css('padding-bottom', footerBlockHeight + 'px');
+	}
 
-    // ------------------------------------------------------ //
-    // For demo purposes, can be deleted
-    // ------------------------------------------------------ //
+	// ------------------------------------------------------- //
+	// External links to new window
+	// ------------------------------------------------------ //
+	$('.external').on('click', function (e) {
 
-    var stylesheet = $('link#theme-stylesheet');
-    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
+		e.preventDefault();
+		window.open($(this).attr("href"));
+	});
 
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
+	// ------------------------------------------------------ //
+	// For demo purposes, can be deleted
+	// ------------------------------------------------------ //
 
-    $("#colour").change(function () {
+	var stylesheet = $('link#theme-stylesheet');
+	$("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
+	var alternateColour = $('link#new-stylesheet');
 
-        if ($(this).val() !== '') {
+	if ($.cookie("theme_csspath")) {
+		alternateColour.attr("href", $.cookie("theme_csspath"));
+	}
 
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
+	$("#colour").change(function () {
 
-            alternateColour.attr("href", theme_csspath);
+		if ($(this).val() !== '') {
 
-            $.cookie("theme_csspath", theme_csspath, {
-                expires: 365,
-                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-            });
+			var theme_csspath = 'css/style.' + $(this).val() + '.css';
 
-        }
+			alternateColour.attr("href", theme_csspath);
 
-        return false;
-    });
+			$.cookie("theme_csspath", theme_csspath, {
+				expires: 365,
+				path: document.URL.substr(0, document.URL.lastIndexOf('/'))
+			});
+
+		}
+
+		return false;
+	});
+
+
 
 });
 
@@ -181,3 +183,92 @@ $(document).ready(function () {
 $('.carousel').carousel({
   interval: 0
 });
+
+// ------------------------------------------------------ //
+// Upload Photo
+// ------------------------------------------------------ //
+// vars
+let result = document.querySelector('.result'),
+img_result = document.querySelector('.img-result'),
+img_w = document.querySelector('.img-w'),
+img_h = document.querySelector('.img-h'),
+options = document.querySelector('.options'),
+save = document.querySelector('.save'),
+cropped = document.querySelector('.cropped'),
+dwn = document.querySelector('.download'),
+upload = document.querySelector('#file-input'),
+cropper = '';
+
+// on change show image with crop options
+upload.addEventListener('change', (e) => {
+  if (e.target.files.length) {
+		// start file reader
+	const reader = new FileReader();
+	reader.onload = (e)=> {
+	  if(e.target.result){
+				// create new image
+				let img = document.createElement('img');
+				img.id = 'image';
+				img.src = e.target.result
+				// clean result before
+				result.innerHTML = '';
+				// append new image
+				result.appendChild(img);
+				// show save btn and options
+				save.classList.remove('hide');
+				options.classList.remove('hide');
+				// init cropper
+				cropper = new Cropper(img);
+	  }
+	};
+	reader.readAsDataURL(e.target.files[0]);
+  }
+});
+
+// save on click
+save.addEventListener('click',(e)=>{
+  e.preventDefault();
+  // get result to data uri
+  let imgSrc = cropper.getCroppedCanvas({
+		width: img_w.value // input value
+	}).toDataURL();
+  // remove hide class of img
+  cropped.classList.remove('hide');
+	img_result.classList.remove('hide');
+	// show image cropped
+  cropped.src = imgSrc;
+  dwn.classList.remove('hide');
+  dwn.download = 'imagename.png';
+  dwn.setAttribute('href',imgSrc);
+});
+
+
+// ------------------------------------------------------ //
+// Edit profile photo preview
+// ------------------------------------------------------ //
+var slideIndex = 1;
+showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].setAttribute("style", "opacity: .6;");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].setAttribute("style", "opacity: 1;");
+}
