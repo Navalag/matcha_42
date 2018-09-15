7f9c7109ae4d993a8dd8ceb3f1ffe8245c5dbdd9
Andrew GALAVAN <agalavan@student.unit.ua>
2018-09-15 17:27:50 +0300

    fix view edit interests

--- 2-front-end/js/front.js
+++ 2-front-end/js/front.js
@@ -279,23 +279,23 @@ $(document).ready(function() {
 	$('input[type="file"]').each(function(){
 	  // Refs
 	  var $file = $(this),
-	      $label = $file.next('label'),
-	      $labelText = $label.find('span'),
-	      labelDefault = $labelText.text();
+		  $label = $file.next('label'),
+		  $labelText = $label.find('span'),
+		  labelDefault = $labelText.text();
 	  // When a new file is selected
 	  $file.on('change', function(event){
-	    var fileName = $file.val().split( '\\' ).pop(),
-	        tmppath = URL.createObjectURL(event.target.files[0]);
-	    //Check successfully selection
+		var fileName = $file.val().split( '\\' ).pop(),
+			tmppath = URL.createObjectURL(event.target.files[0]);
+		//Check successfully selection
 			if( fileName ){
-	      $label
-	        .addClass('file-ok')
-	        .css('background-image', 'url(' + tmppath + ')');
+		  $label
+			.addClass('file-ok')
+			.css('background-image', 'url(' + tmppath + ')');
 				$labelText.text(fileName);
-	    }else{
-	      $label.removeClass('file-ok');
+		}else{
+		  $label.removeClass('file-ok');
 				$labelText.text(labelDefault);
-	    }
+		}
 	  });
 	  
 	// End loop of file input elements  
@@ -322,97 +322,101 @@ $('textarea.form-control').keyup(function() {
 
 $(document).ready(function() {
 
-    var select = $('select[multiple]');
-    var options = select.find('option');
+	var select = $('select[multiple]');
+	var options = select.find('option');
 
-    var div = $('<div />').addClass('selectMultiple');
-    var active = $('<div />');
-    var list = $('<ul />');
-    var placeholder = select.data('placeholder');
+	var div = $('<div />').addClass('selectMultiple');
+	var active = $('<div />');
+	var list = $('<ul />');
+	var placeholder = select.data('placeholder');
 
-    var span = $('<span />').text(placeholder).appendTo(active);
+	var span = $('<span />').text(placeholder).appendTo(active);
 
-    options.each(function() {
-        var text = $(this).text();
-        if($(this).is(':selected')) {
-            active.append($('<a />').html('<em>' + text + '</em><i></i>'));
-            span.addClass('hide');
-        } else {
-            list.append($('<li />').html(text));
-        }
-    });
+	options.each(function() {
+		var text = $(this).text();
+		if($(this).is(':selected')) {
+			active.append($('<a />').html('<em>' + text + '</em><i></i>'));
+			span.addClass('hide');
+		} else {
+			list.append($('<li />').html(text));
+		}
+	});
 
-    active.append($('<div />').addClass('arrow'));
-    div.append(active).append(list);
+	active.append($('<div />').addClass('arrow'));
+	div.append(active).append(list);
 
-    select.wrap(div);
+	select.wrap(div);
 
-    $(document).on('click', '.selectMultiple ul li', function(e) {
-        var select = $(this).parent().parent();
-        var li = $(this);
-        li.prev().addClass('beforeRemove');
-        li.next().addClass('afterRemove');
-        li.addClass('remove');
-        var a = $('<a />').addClass('notShown').html('<em>' + li.text() + '</em><i></i>').hide().appendTo(select.children('div'));
-        a.slideDown(400, function() {
-            setTimeout(function() {
-                a.addClass('shown');
-                select.children('div').children('span').addClass('hide');
-                select.find('option:contains(' + li.text() + ')').prop('selected', true);
-            }, 500);
-        });
-        setTimeout(function() {
-            if(li.prev().is(':last-child')) {
-                li.prev().removeClass('beforeRemove');
-            }
-            if(li.next().is(':first-child')) {
-                li.next().removeClass('afterRemove');
-            }
-            setTimeout(function() {
-                li.prev().removeClass('beforeRemove');
-                li.next().removeClass('afterRemove');
-            }, 200);
+	$(document).on('click', '.selectMultiple ul li', function(e) {
+		var select = $(this).parent().parent();
+		var li = $(this);
+		li.prev().addClass('beforeRemove');
+		li.next().addClass('afterRemove');
+		li.addClass('remove');
+		var a = $('<a />').addClass('notShown').html('<em>' + li.text() + '</em><i></i>').hide().appendTo(select.children('div'));
+		a.slideDown(400, function() {
+			setTimeout(function() {
+				a.addClass('shown');
+				select.children('div').children('span').addClass('hide');
+				select.find('option:contains(' + li.text() + ')').prop('selected', true);
+			}, 500);
+		});
+		setTimeout(function() {
+			if(li.prev().is(':last-child')) {
+				li.prev().removeClass('beforeRemove');
+			}
+			if(li.next().is(':first-child')) {
+				li.next().removeClass('afterRemove');
+			}
+			setTimeout(function() {
+				li.prev().removeClass('beforeRemove');
+				li.next().removeClass('afterRemove');
+			}, 200);
 
-            li.slideUp(400, function() {
-                li.remove();
-            });
-        }, 600);
-    });
+			li.slideUp(400, function() {
+				li.remove();
+			});
+		}, 600);
 
-    $(document).on('click', '.selectMultiple > div a', function(e) {
-        var select = $(this).parent().parent();
-        var self = $(this);
-        self.removeClass().addClass('remove');
-        select.addClass('open');
-        setTimeout(function() {
-            self.addClass('disappear');
-            setTimeout(function() {
-                self.animate({
-                    width: 0,
-                    height: 0,
-                    padding: 0,
-                    margin: 0
-                }, 300, function() {
-                    var li = $('<li />').text(self.children('em').text()).addClass('notShown').appendTo(select.find('ul'));
-                    li.slideDown(400, function() {
-                        li.addClass('show');
-                        setTimeout(function() {
-                            select.find('option:contains(' + self.children('em').text() + ')').prop('selected', false);
-                            if(!select.find('option:selected').length) {
-                                select.children('div').children('span').removeClass('hide');
-                            }
-                            li.removeClass();
-                        }, 400);
-                    });
-                    self.remove();
-                })
-            }, 300);
-        }, 400);
-    });
+		console.log(li.text());
+	});
 
-    $(document).on('click', '.selectMultiple > div .arrow, .selectMultiple > div span', function(e) {
-        $(this).parent().parent().toggleClass('open');
-    });
+	$(document).on('click', '.selectMultiple > div a', function(e) {
+		var select = $(this).parent().parent();
+		var self = $(this);
+		self.removeClass().addClass('remove');
+		select.addClass('open');
+		setTimeout(function() {
+			self.addClass('disappear');
+			setTimeout(function() {
+				self.animate({
+					width: 0,
+					height: 0,
+					padding: 0,
+					margin: 0
+				}, 300, function() {
+					var li = $('<li />').text(self.children('em').text()).addClass('notShown').appendTo(select.find('ul'));
+					li.slideDown(400, function() {
+						li.addClass('show');
+						setTimeout(function() {
+							select.find('option:contains(' + self.children('em').text() + ')').prop('selected', false);
+							if(!select.find('option:selected').length) {
+								select.children('div').children('span').removeClass('hide');
+							}
+							li.removeClass();
+						}, 400);
+					});
+					self.remove();
+				})
+			}, 300);
+		}, 400);
+		
+		console.log(self.children('em').text());
+	});
+
+	$(document).on('click', '.selectMultiple > div .arrow, .selectMultiple > div span', function(e) {
+		$(this).parent().parent().toggleClass('open');
+	});
 
 });
 
